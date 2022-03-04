a93547eb3b72931210f5b85f0f0c820bb86c458e
Kobe Chenea <cheneak@flip2.engr.oregonstate.edu>
2022-03-03 17:33:37 -0800

    updated app.py

--- app.py
+++ app.py
@@ -1,32 +1,20 @@
-from flask import Flask, render_template, json, redirect
-from flask_mysqldb import MySQL
-from flask import request
+from flask import Flask, render_template
 import os
+
+# Configuration
 
 app = Flask(__name__)
 
-app.config['MYSQL_HOST'] = 'classmysql.engr.oregonstate.edu'
-app.config['MYSQL_USER'] = 'cs340_cheneak'
-app.config['MYSQL_PASSWORD'] = '7051' #last 4 of onid
-app.config['MYSQL_DB'] = 'cs340_cheneak'
-app.config['MYSQL_CURSORCLASS'] = "DictCursor"
+# Routes 
 
-
-mysql = MySQL(app)
-
-
-# Routes
 @app.route('/')
 def root():
-    query = "SELECT * FROM diagnostic;"
-    cur = mysql.connection.cursor()
-    cur.execute(query)
-    results = cur.fetchall()
-
-    return results[0]
-
+	
+	return render_template('index.html')
 
 # Listener
-if __name__ == "__main__":
 
-    app.run(port=8766, debug=True)
+if __name__ == "__main__":
+    
+	app.run(host="0.0.0.0", port=8766, debug=True) 
+
